28c3670c8712a57b29bb680e88819e1526480c3e
EC2 Default User <ec2-user@ip-172-16-34-45.us-east-2.compute.internal>
2019-10-03 17:25:06 +0000

    Updated notebook

--- ems_call_prediction/ems_call_prediction.py
+++ ems_call_prediction/ems_call_prediction.py
@@ -13,8 +13,31 @@ from pandas.tseries.holiday import AbstractHolidayCalendar
 from pandas.tseries.offsets import CustomBusinessDay
 from pandas.tseries.holiday import Holiday
 import datetime
+from io import StringIO
 from sklearn.externals import joblib
 import numpy as np
+
+
+#Feature list
+feature_list = ['high_temp', 'precip_intensity',
+       'description.day_of_week_Friday', 'description.day_of_week_Monday',
+       'description.day_of_week_Saturday', 'description.day_of_week_Sunday',
+       'description.day_of_week_Thursday', 'description.day_of_week_Tuesday',
+       'description.day_of_week_Wednesday',
+       'fire_department.firecares_id_74731',
+       'fire_department.firecares_id_81154',
+       'fire_department.firecares_id_93345',
+       'fire_department.firecares_id_98606', 'month_01', 'month_02',
+       'month_03', 'month_04', 'month_05', 'month_06', 'month_07', 'month_08',
+       'month_09', 'month_10', 'month_11', 'month_12', 'precip_type_none',
+       'precip_type_rain', 'precip_type_sleet', 'precip_type_snow',
+       'holiday_Christmas', 'holiday_Christmas Eve', 'holiday_Columbus Day',
+       'holiday_DST time change', 'holiday_Dr. Martin Luther King Jr.',
+       'holiday_Halloween', 'holiday_July 4th', 'holiday_Labor Day',
+       'holiday_MemorialDay', 'holiday_New Years Day', 'holiday_New Years Eve',
+       'holiday_Presidents Day', 'holiday_Thanksgiving',
+       'holiday_Veterans Day', 'holiday_none']  
+
 
 
 if __name__ == '__main__':
@@ -35,7 +58,6 @@ if __name__ == '__main__':
     with open(dataloc) as data_file:
         data = json.load(data_file)
     features = pd.io.json.json_normalize(data['prediction_data'])
-
     
     #Converting df dates to datetime objects for holiday merging
     features['date'] = features.apply(lambda x: datetime.datetime.strptime(x['date'],'%Y-%m-%d'),axis=1)
@@ -73,8 +95,9 @@ if __name__ == '__main__':
     #Splitting the data into features (predictors) and labels (the quantity we want to predict)
     labels = features['ems_calls']
     features = features.drop('ems_calls',axis=1)
-
-
+    
+    #Making sure the column order is correct
+    features = features[feature_list]
 
     # Here we support a single hyperparameter, 'max_leaf_nodes'. Note that you can add as many
     # as your training my require in the ArgumentParser above.
@@ -89,6 +112,62 @@ if __name__ == '__main__':
 #     # Print the coefficients of the trained classifier, and save the coefficients
     joblib.dump(rf, os.path.join(args.model_dir, "model.joblib"))
 
+    
+def input_fn(input_data, content_type):
+    """Parse input data payload
+
+    We currently only take csv input. Since we need to process both labelled
+    and unlabelled data we first determine whether the label column is present
+    by looking at how many columns were provided.
+    """
+    
+   
+    data = json.loads(input_data)
+    features = pd.io.json.json_normalize(data['prediction_data'])
+    
+    #Converting df dates to datetime objects for holiday merging
+    features['date'] = features.apply(lambda x: datetime.datetime.strptime(x['date'],'%Y-%m-%d'),axis=1)
+    
+    #We can create new rules with this class inheritance
+    class custom_calendar(AbstractHolidayCalendar):
+        new_rules = [
+            Holiday('Halloween', month=10,day=31),
+            Holiday('Christmas Eve', month=12,day=24),
+            Holiday('New Years Eve', month=12,day=31),
+            Holiday('DST time change', month=3, day=1, offset=pd.DateOffset(weekday=SU(2)))
+        ]
+        rules = calendar().rules + new_rules
+
+    cal = custom_calendar()
+    start = np.min(features['date'])
+    end = np.max(features['date'])
+    holidays = cal.holidays(start=start,end=end,return_name=True).reset_index()
+    holidays = holidays.rename(columns={'index':'date', 0:'holiday'})
+
+    #But really we want the Monday after the time change
+    holidays['date'] = holidays.apply(lambda x: x['date'] + datetime.timedelta(days=1) 
+                   if x['holiday'] == 'DST time change' else x['date'], axis=1)
+
+    features = features.merge(holidays, on='date', how='left')
+    features['holiday'] = features['holiday'].fillna('none')
+    #Filling missing values with the average
+    features['high_temp'] = features['high_temp'].fillna(np.mean(features['high_temp']))
+
+    #No longer need the date since we have all the information we need (day of week and month)
+    features = features.drop('date',axis=1)
+    #Using one hot encoding for categorical variables. Ask me if you want me to explain this further.
+    features = pd.get_dummies(features)
+    
+
+    
+    for feature in feature_list:
+        if feature not in features.columns:
+            features[feature] = 0
+            
+    #Making sure the column order is correct
+    features = features[feature_list]
+    
+    return features
 
 def model_fn(model_dir):
     """Deserialized and return fitted model
