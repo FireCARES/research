45b172d19de69e552475808e770805551183f592
Luca DeCaprio <luca.decaprio@prominentedge.com>
2020-04-28 22:18:33 -0400

    Changed way access property

--- move_up_model/handler.py
+++ move_up_model/handler.py
@@ -4,7 +4,8 @@ from move_ups import move_up_model
 def move_up(event, context):
     try:
         print(event)
-        model = move_up_model(event.body)
+        print(event['body'])
+        model = move_up_model(event['body'])
         return {
             "statusCode": 200,
             "body": json.dumps(model.output)
