887403590a66e885c73791be0b52ac6894462d9a
Luca DeCaprio <luca.decaprio@prominentedge.com>
2020-05-04 19:44:03 -0400

    Removed unnecessary config

--- move_up_model/handler.py
+++ move_up_model/handler.py
@@ -2,20 +2,23 @@ import json
 from move_ups import move_up_model
 
 def move_up(event, context):
+    headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': True,
+    }
+
     try:
         payload = json.loads(event['body'])
         model = move_up_model(payload)
         return {
             "statusCode": 200,
-            "headers": {
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Credentials': 'true',
-            },
+            "headers": headers,
             "body": json.dumps(model.output)
         }
     except Exception as error:
         print(error)
         return {
-            "statusCode": 500
+            "statusCode": 500,
+            "headers": headers,
         }
 
