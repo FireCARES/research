dc4d98597177765b4973943ae20e4745492f5a73
Luca DeCaprio <luca.decaprio@prominentedge.com>
2020-05-04 18:46:46 -0400

    added content type to response headers

--- move_up_model/handler.py
+++ move_up_model/handler.py
@@ -9,6 +9,7 @@ def move_up(event, context):
             "statusCode": 200,
             "headers": {
                 "Access-Control-Allow-Origin" : "*",
+                "Content-Type": "application/json"
             },
             "body": json.dumps(model.output)
         }
