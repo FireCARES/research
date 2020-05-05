0240abe9ed30878a5a8ac0178a2eaf4c652345b3
Luca DeCaprio <luca.decaprio@prominentedge.com>
2020-05-04 20:09:00 -0400

    Added error response

--- move_up_model/handler.py
+++ move_up_model/handler.py
@@ -17,8 +17,13 @@ def move_up(event, context):
         }
     except Exception as error:
         print(error)
+        error = {
+            error: error
+        }
+
         return {
             "statusCode": 500,
             "headers": headers,
+            "body": json.dumps(error)
         }
 
